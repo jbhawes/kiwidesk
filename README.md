645a4c11524f0e97f25e5648df29565f49f031ac
jbhawes <james@jameshawes.net>
2016-10-14 17:37:09 -0500

    initial upload
    Desktop app built with Electron for BeagleBone Green project

---
+++ README.md
@@ -0,0 +1,29 @@
+# KiwiDesk
+
+> My KiwiDesk app built with Electron
+
+
+## Dev
+
+```
+$ npm install
+```
+
+### Run
+
+```
+$ bozon start
+```
+
+### Package
+
+```
+$ bozon package
+```
+
+Builds the app for OS X, Linux, and Windows, using [electron-builder](https://github.com/electron-userland/electron-builder).
+
+
+## License
+
+The MIT License (MIT) © James Hawes 2016
---
+++ app/javascripts/renderer/application.js
@@ -0,0 +1,11 @@
+require('electron').ipcRenderer.on('loaded' , function(event, data) {
+  // document.getElementById('title').innerHTML = data.appName + ' App';
+  // document.getElementById('details').innerHTML = 'built with Electron v' + data.electronVersion;
+  // document.getElementById('versions').innerHTML = 'running on Node v' + data.nodeVersion + ' and Chromium v' + data.chromiumVersion;
+
+  $(".btn-black").on("click", function(){
+    var dataid = $(this).data("btnid");
+    alert('here: ' + dataid);
+  });
+
+});
